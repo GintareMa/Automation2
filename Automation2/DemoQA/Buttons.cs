2f7c6f226dab602339cf4fe881bcb4c18c8ae64a
gintare.mazeikyte <gintare.mazeikyte@zenitech.co.uk>
2022-06-27 21:46:23 +0300

    New test leftClick and rightClick

---
+++ Automation2/DemoQA/Buttons.cs
@@ -0,0 +1,49 @@
+﻿using Framework;
+using Framework.Pages.DemoQA;
+using NUnit.Framework;
+
+namespace Automation2.DemoQA
+{
+    public class Buttons
+    {
+        [SetUp]
+
+        public static void setup()
+        {
+            Driver.setDriver();
+            Driver.open("https://demoqa.com/buttons");
+        }
+
+        [Test]
+
+        public static void leftClick()
+        {
+            string expectedMessage = "You have done a dynamic click";
+            string actualMessage;
+            ButtonsPage.leftClick();
+            actualMessage = ButtonsPage.readMessageLeftClick();
+
+            Assert.AreEqual(expectedMessage, actualMessage);
+        }
+
+        [Test]
+
+        public static void rightClick()
+        {
+            string expectedMessage = "You have done a right click";
+            string actualMessage;
+
+            ButtonsPage.rightClick();
+            actualMessage= ButtonsPage.readMessageRightClick();
+
+            Assert.AreEqual(expectedMessage, actualMessage);
+
+        }
+
+        [TearDown]
+        public static void close()
+        {
+            Driver.closeDriver();
+        }
+    }
+}
---
+++ Framework/Pages/DemoQA/ButtonsPage.cs
@@ -0,0 +1,31 @@
+﻿namespace Framework.Pages.DemoQA
+{
+    public class ButtonsPage
+    {
+        public static void leftClick()
+        {
+            string locator = "//*[text()='Click Me']";
+            //string locator = "//*[contains(text(),'Click Me')]";
+            Common.clickElement(locator);
+        }
+
+        public static string readMessageLeftClick()
+        {
+            string locator = "//*[@id='dynamicClickMessage']";
+            return Common.getElementText(locator); 
+        }
+
+        public static void rightClick()
+        {
+            string locator = "//*[@id='rightClickBtn']";
+            Common.performRightClick(locator);
+        }
+
+        public static string readMessageRightClick()
+        {
+            string locator = "//*[@id='rightClickMessage']";
+            return Common.getElementText(locator);
+
+        }
+    }
+}
