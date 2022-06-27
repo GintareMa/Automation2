a87a4df40ed7eba43591bd0290f9728367218113
gintare.mazeikyte <gintare.mazeikyte@zenitech.co.uk>
2022-06-27 21:48:10 +0300

    new test singleSelect

---
+++ Automation2/SeleniumEasy/BasicSelectDropDownDemo.cs
@@ -0,0 +1,37 @@
+﻿using Framework;
+using Framework.Pages.SeleniumEasy;
+using NUnit.Framework;
+
+namespace Automation2.SeleniumEasy
+{
+    public class BasicSelectDropDownDemo
+    {
+        [SetUp]
+
+        public static void setup()
+        {
+            Driver.setDriver();
+            Driver.open("https://demo.seleniumeasy.com/basic-select-dropdown-demo.html");
+        }
+
+        [Test]
+
+        public static void singleSelect()
+        {
+            string expectedDay = "Tuesday";
+            string aactualMessage;
+            BasicSelectDropDownDemoPage.selectDayByValue(expectedDay);
+            aactualMessage = BasicSelectDropDownDemoPage.readSelectedDay();
+        
+            Assert.IsTrue(aactualMessage.Contains(expectedDay), 
+                $"Expected '{aactualMessage}' to contain value: '{expectedDay}'");    
+        }
+
+        [TearDown]
+        public static void close()
+        {
+            Driver.closeDriver();
+        }
+    }
+
+}
---
+++ Framework/Pages/SeleniumEasy/BasicSelectDropDownDemoPage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Framework.Pages.SeleniumEasy
+{
+    public class BasicSelectDropDownDemoPage
+    {
+        public static void selectDayByValue(string value)
+        {
+            string locator = "//*[@id='select-demo']";
+            Common.selectOptionByValue(locator, value);
+        }
+
+        public static string readSelectedDay()
+        {
+            string locator = "//*[@class='selected-value']";
+            return Common.getElementText(locator);
+        }
+    }
+}
