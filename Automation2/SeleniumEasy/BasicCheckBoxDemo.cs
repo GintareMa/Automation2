cb99dd891a9f060ad0081bc840bc9136c51c2ef1
gintare.mazeikyte <gintare.mazeikyte@zenitech.co.uk>
2022-06-27 21:47:07 +0300

    new test singleCheckBox

---
+++ Automation2/SeleniumEasy/BasicCheckBoxDemo.cs
@@ -0,0 +1,38 @@
+﻿using Framework;
+using Framework.Pages.SeleniumEasy;
+using NUnit.Framework;
+
+namespace Automation2.SeleniumEasy
+{
+    public class BasicCheckBoxDemo
+    {
+        [SetUp]
+
+        public static void setup()
+        {
+            Driver.setDriver();
+            Driver.open("https://demo.seleniumeasy.com/basic-checkbox-demo.html");
+        }
+
+        [Test]
+
+        public static void singleCheckBox()
+        {
+            string expectedMessage = "Success - Check box is checked";
+            string actualMessage = "";
+            BasicCheckBoxDemoPage.clickSingleCheckbox();
+            actualMessage = BasicCheckBoxDemoPage.readMessage();
+
+            Assert.AreEqual(expectedMessage, actualMessage);
+        }
+
+
+
+        [TearDown]
+        public static void close()
+        {
+            Driver.closeDriver();
+        }
+    }
+}
+
